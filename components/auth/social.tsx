5022f33571743498af7b42e64c1433c42156f935
Aman <amangarg0303@gmail.com>
2024-01-13 23:19:19 +0530

    github and google login

--- components/auth/social.tsx
+++ components/auth/social.tsx
@@ -1,11 +1,24 @@
 "use client";
 
+import { signIn } from "next-auth/react";
 import { Button } from "@/components/ui/button";
+import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
 
 export const Social = () => {
+  const onClick = (provider: "google" | "github") => {
+    signIn(provider, {
+      callbackUrl: DEFAULT_LOGIN_REDIRECT,
+    });
+  };
+
   return (
     <div className="flex items-center w-full gap-x-2">
-      <Button size="lg" className="w-full" variant="outline" onClick={() => {}}>
+      <Button
+        size="lg"
+        className="w-full"
+        variant="outline"
+        onClick={() => onClick("google")}
+      >
         <svg
           width="20"
           height="20"
@@ -31,7 +44,12 @@ export const Social = () => {
           />
         </svg>
       </Button>
-      <Button size="lg" className="w-full" variant="outline" onClick={() => {}}>
+      <Button
+        size="lg"
+        className="w-full"
+        variant="outline"
+        onClick={() => onClick("github")}
+      >
         <svg
           viewBox="0 0 256 250"
           width="20"
