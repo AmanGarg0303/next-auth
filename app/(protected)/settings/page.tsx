68dbecedcc8a4013b7671eab6969de35566c8aee
Aman <amangarg0303@gmail.com>
2024-01-16 16:26:01 +0530

    minor bug fix

--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -49,6 +49,7 @@ export default function SettingsPage() {
       password: undefined,
       newPassword: undefined,
       role: user?.role || undefined,
+      isTwoFactorEnabled: user?.isTwoFactorEnabled || undefined,
     },
   });
 
